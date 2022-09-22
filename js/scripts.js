7494a401689ad625771416d934c2affb417439cf
Zalmy L <shneurlieberman@gmail.com>
2022-09-22 13:35:17 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+/* Here is a basic alert JS pop up and an example of multi line comment
+alert ('Hello World');8
+*/
+
+//Here is an example of a variable overwirting a prev one and a 1 line comment.
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+
+//Math example using JS for + numebrs
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+
+//Math example using JS for multipliing numbers and sizing
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+
