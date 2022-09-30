086dd20a0e21e38ffd0be8bc0674cf8f949cbeb5
Zalmy L <shneurlieberman@gmail.com>
2022-09-29 22:53:14 -0400

    adding my my loop and conditional function to my pokeman app

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,46 @@
 let pokemonList = [
-    { name: "Charmeleon", height: "1", type: ["grass","water", "speed"] },
+    { name: "Charmeleon", height: "1", type: ["grass", "water", "speed"] },
     { name: "Spearow", height: "3", type: "water" },
     { name: "Sandslash", height: "2", type: "speed" },
 ];
 
+//the "for" will start the loop, the "let i=0" will set loop to start from "0". The pokemonList.length will go through the length of my PokemonList
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 0 && pokemonList[i].height < 2) {
+        document.write(
+            "<h1>" +
+                pokemonList[i].name +
+                " (height: " +
+                pokemonList[i].height +
+                ") - This is a small Pokepmon!)</h1>"
+        );
+    } else if (pokemonList[i].height > 0.3 && pokemonList[i].height < 3) {
+        document.write(
+            "<h1>" +
+                pokemonList[i].name +
+                " (height: " +
+                pokemonList[i].height +
+                ") - This is a medium sized Pokemon!)</h1>"
+        );
+    } else {
+        document.write(
+            "<h1>" +
+                pokemonList[i].name +
+                " (height: " +
+                pokemonList[i].height +
+                ") - This is a large pokemon!)</h1>"
+        );
+    }
+}
 
 
 
-/*leavin gthis here for a placeholder ofr now will remove when i have time to add these to my personal notes */
+
+
+
+    
+    /*leavin gthis here for a placeholder ofr now will remove when i have time to add these to my personal notes */
 /*let pokemon1 = {name:"Charmeleon", height:"1.1 m", type:"grass" };*/
 /*
 //Here is an example of a variable overwirting a prev one and a 1 line comment.
