2b3ee93bea9cb2112e054bdb4f7276c2e41833ca
Zalmy L <shneurlieberman@gmail.com>
2022-10-20 14:56:46 -0400

    Added IFEE and add for task

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,76 @@
-let pokemonList = [
-    { name: "Charmeleon", height: "1", type: ["grass", "water", "speed"] },
-    { name: "Spearow", height: "3", type: "water" },
-    { name: "Sandslash", height: "2", type: "speed" },
-];
+let pokemanRepository = (function () {
+    let pokemonList = [
+        { name: "Charmeleon", height: "1", type: ["grass", "water", "speed"] },
+        { name: "Spearow", height: "3", type: "water" },
+        { name: "Sandslash", height: "2", type: "speed" },
+    ];
 
-pokemonList.forEach ( item => console.log (item))
+    let getAll = function () {
+        return pokemonList
+    };
 
-//the "for" will start the loop, the "let i=0" will set loop to start from "0". The pokemonList.length will go through the length of my PokemonList
+    let add = function (item) {
+        
+        if(typeof item == "object"){
+            pokemonList.push (item);  
+        }
+    };
+    
+    return {
+        getAll: getAll,
+        add: add
+    }
+}) ();
 
-for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height > 0 && pokemonList[i].height < 2) {
+pokemanRepository.getAll().forEach ( item => {
+    
+    if (item.height > 0 && item.height < 2) {
         document.write(
             "<h1>" +
-                pokemonList[i].name +
+                item.name +
                 " (height: " +
-                pokemonList[i].height +
+                item.height +
                 ") - This is a small Pokepmon!)</h1>"
         );
-    } else if (pokemonList[i].height > 0.3 && pokemonList[i].height < 3) {
+    } else if (item.height > 0.3 && item.height < 3) {
         document.write(
             "<h1>" +
-                pokemonList[i].name +
+            item. name +
                 " (height: " +
-                pokemonList[i].height +
+                item .height +
                 ") - This is a medium sized Pokemon!)</h1>"
         );
     } else {
         document.write(
             "<h1>" +
-                pokemonList[i].name +
+            item.name +
                 " (height: " +
-                pokemonList[i].height +
+                item.height +
                 ") - This is a large pokemon!)</h1>"
         );
     }
-}
+});
+
+
+// // expect this to log the pokeman list
+// console.log (pokemanRepository.getAll());
+// console.log ("pokemanList size is " + pokemanRepository.getAll().length);
+// // adding 1 pokeman object to the list
+// pokemanRepository.add ( { name: "Kane", height: "5", type: "nudnick" })
+// // expect this to log the pokeman list
+// console.log (pokemanRepository.getAll());
+// console.log ("pokemanList size is " + pokemanRepository.getAll().length);
+
+
+//pokemonList.forEach ( item => console.log (item))
 
 
 
+//the "for" will start the loop, the "let i=0" will set loop to start from "0". The pokemonList.length will go through the length of my PokemonList
 
 
 
-    
-    /*leavin gthis here for a placeholder ofr now will remove when i have time to add these to my personal notes */
+    /*leaving this here for a placeholder for now will remove when i have time to add these to my personal notes */
 /*let pokemon1 = {name:"Charmeleon", height:"1.1 m", type:"grass" };*/
 /*
 //Here is an example of a variable overwirting a prev one and a 1 line comment.
