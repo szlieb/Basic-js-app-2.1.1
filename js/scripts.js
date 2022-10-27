4ffbd6ade33ee0dceb51ef976d99afd2bdf6498e
Zalmy L <shneurlieberman@gmail.com>
2022-10-27 13:41:43 -0400

    1st submission for FSE 1.6 DOM manipulation

--- js/scripts.js
+++ js/scripts.js
@@ -1,95 +1,95 @@
-let pokemanRepository = (function () {
-    let pokemonList = [
-        { name: "Charmeleon", height: "1", type: ["grass", "water", "speed"] },
-        { name: "Spearow", height: "3", type: "water" },
-        { name: "Sandslash", height: "2", type: "speed" },
+let pokemonRepository=(function () {
+    let repository=[ {
+        name: "Charmeleon", height: 1, types: ["grass", "water", "speed"],
+    }
+
+    ,
+        {
+        name: "Spearow", height: 3, types: ["water"],
+    }
+
+    ,
+        {
+        name: "Sandslash", height: 2, types: ["speed"],
+    }
+
+    ,
     ];
 
-    let getAll = function () {
-        return pokemonList
-    };
-
-    let add = function (item) {
-        
-        if(typeof item == "object"){
-            pokemonList.push (item);  
+    // Creating the function to set loop of if's
+    function add(pokemon) {
+        if (typeof pokemon==="object"&& "name"in pokemon && "height"in pokemon && "types"in pokemon) {
+            //This code will push pokemon list to the function
+            repository.push(pokemon);
         }
-    };
-    
+
+        else {
+            console.log("pokemon is not correct");
+        }
+    }
+
+    //This should return all that is in the variable pokemonList
+    function getAll() {
+        return repository;
+    }
+
+    function addListItem(pokemon) {
+        //target the class pokemon-list in the HTML file
+        let pokemonList=document.querySelector(".pokemon-list");
+        //Creat a list itmes on the ul class pokemon-list 
+        let listpokemon=document.createElement("li");
+        //creates buttons out of the UL list
+        let button=document.createElement("button");
+        // should i add the event listner here ?  addPokemonListener(button, pokemon);
+        // pass in the pokemon names in the inner text of the button
+        button.innerText=pokemon.name;
+        /*DO  I PUT THE CSS LINK HERE? is this correct for the css linking line 47?*/
+        button.classList.add("button-class"); 
+        //pass the li into the button on the webpage
+        listpokemon.appendChild(button);
+        pokemonList.appendChild(listpokemon);
+    }
+
+   // creating this function to be used later on Is this the correct way to add event listner? see line 43 
+    function addPokemonListener (button, pokemon) {
+        button.addEventListener('click', function() {
+                showDetails(pokemon);
+        }
+
+        )
+    }
+
+    //Add show details function
+    function showDetails (pokemon) {
+        console.log(pokemon.name);
+    }
+
     return {
+        //Return added items
+        add: add,
+        //get all items
         getAll: getAll,
-        add: add
+        //Return the new list items
+        addListItem: addListItem
     }
-}) ();
 
-pokemanRepository.getAll().forEach ( item => {
-    
-    if (item.height > 0 && item.height < 2) {
-        document.write(
-            "<h1>" +
-                item.name +
-                " (height: " +
-                item.height +
-                ") - This is a small Pokepmon!)</h1>"
-        );
-    } else if (item.height > 0.3 && item.height < 3) {
-        document.write(
-            "<h1>" +
-            item. name +
-                " (height: " +
-                item .height +
-                ") - This is a medium sized Pokemon!)</h1>"
-        );
-    } else {
-        document.write(
-            "<h1>" +
-            item.name +
-                " (height: " +
-                item.height +
-                ") - This is a large pokemon!)</h1>"
-        );
-    }
-});
+    ;
+}
 
+)();
 
-// // expect this to log the pokeman list
-// console.log (pokemanRepository.getAll());
-// console.log ("pokemanList size is " + pokemanRepository.getAll().length);
-// // adding 1 pokeman object to the list
-// pokemanRepository.add ( { name: "Kane", height: "5", type: "nudnick" })
-// // expect this to log the pokeman list
-// console.log (pokemanRepository.getAll());
-// console.log ("pokemanList size is " + pokemanRepository.getAll().length);
+//Adding new pokemon to my array
+pokemonRepository.add( {
+    name: "Kane", height: 0.5, types: ["nudnick"]
+}
 
+);
+console.log(pokemonRepository.getAll()); //log array with new item
 
-//pokemonList.forEach ( item => console.log (item))
+//function refrence
+pokemonRepository.getAll().forEach(function (pokemon) {
+    //Making the call to get the list items
+    pokemonRepository.addListItem(pokemon);
+}
 
-
-
-//the "for" will start the loop, the "let i=0" will set loop to start from "0". The pokemonList.length will go through the length of my PokemonList
-
-
-
-    /*leaving this here for a placeholder for now will remove when i have time to add these to my personal notes */
-/*let pokemon1 = {name:"Charmeleon", height:"1.1 m", type:"grass" };*/
-/*
-//Here is an example of a variable overwirting a prev one and a 1 line comment.
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
-
-
-//Math example using JS for + numebrs
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-
-//Math example using JS for multipliing numbers and sizing
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize);
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
-*/
-
+);
