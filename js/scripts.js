41f89e6fdc2a36eabd196cfdb5c747ceb7e59408
Zalmy L <shneurlieberman@gmail.com>
2022-11-06 13:14:25 -0500

    Js for FSE 1.7 Updated html & css for loader

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,92 @@
-let pokemonRepository=(function () {
-    let repository=[ {
-        name: "Charmeleon", height: 1, types: ["grass", "water", "speed"],
+let pokemonRepository = (function () {
+	// this will store all our pokamon items
+    let pokemonList = [];
+	// this saves the api url to get the LIST of pokamon items to a variable
+    let listApiUrl="https://pokeapi.co/api/v2/pokemon/?limit=150";
+	// this saves loader DOM element so we can hide and show it when necessary
+    let loader=document.getElementById("loading");
+
+	// calling this will show the loader
+    function showLoader(){
+        loader.classList.add ("loading-show");
+        loader.classList.remove ("loading-hide");
+    }
+	
+	// calling this will hide the loader
+    function hideLoader(){
+        loader.classList.add ("loading-hide");
+        loader.classList.remove ("loading-show");
     }
 
-    ,
-        {
-        name: "Spearow", height: 3, types: ["water"],
+	// this simply adds whatever the passed "item" is into the above "pokemonList" variable
+    function add(item) {
+        pokemonList.push(item)
     }
 
-    ,
-        {
-        name: "Sandslash", height: 2, types: ["speed"],
+	// loads a all the pokomon items returned with calling the "listApiUrl" variable above
+	// we are making sure to hide the loaded once complete whether sucessfully or failure (see catch)
+	function loadList() {
+        showLoader();
+		// fetch will make the http request to "https://pokeapi.co/api/v2/pokemon/?limit=150"
+        return fetch (listApiUrl).then(function(response) {      
+			// at this point the request has been made and we are getting a response. 
+			// we are taking whatever the response.json() returns and passing it along to the next function
+            return response.json();
+        }).then(function(json){
+			// at this point we have our json blob, now we do not want the "count", "prev" or "next" fields. 
+			// all we care about is the "results" field which will contain 150 pokamon items.
+            let results= json.results;
+            results.forEach(function(item) {
+                // api returns "url" as the name of the details url. so will change it to be "detailsUrl" as requested in the course notes.
+                let tranformed=transformListItem(item);
+                add(tranformed);
+            })
+            console.log(pokemonList);
+            hideLoader();
+        }).catch(function(e) {
+            hideLoader();
+            console.error(e);
+        });
     }
 
-    
-    ];
+	// updates a existing pokomon item in our list by adding to fields to it. height and imgUrl.
+    // we are making sure to hide the loaded once complete whether sucessfully or failure (see catch)
+    function loadDetail(item) {
+        showLoader();
+		// call the detail url to get the height and imgurl
+        return fetch (item.detailsUrl).then(function(response) {      
+			// at this point the request has been made and we are getting a response. 
+			// we are taking whatever the response.json() returns and passing it along to the next function
+            return response.json();
+        }).then(function(json) {
+			// the json will contain lots of data. all we care about is the height and imgUrl which are choosing the first image in the sprites array.
+			// we are updating the pokomon item here. we are adding two new fields to it - height and imgUrl
+            item.height=json.height;
+            item.imgUrl=json.sprites.back_default;
+            showDetails(item);
+            hideLoader();
+        }).catch(function(e) {
+            hideLoader();
+            console.error(e);
+        });
+    }
 
-    // Creating the function to set loop of if's
-    function add(pokemon) {
-        if (typeof pokemon==="object"&& "name"in pokemon && "height"in pokemon && "types"in pokemon) {
-            //This code will push pokemon list to the function
-            repository.push(pokemon);
+    // api returns "url" as the name of the details url. so will change it to be "detailsUrl" as requested in the course notes.
+    function transformListItem(item){
+        return {
+            name: item.name,
+            detailsUrl: item.url
         }
-
-        else {
-            console.log("pokemon is not correct");
-        }
     }
+  
+  
 
     //This should return all that is in the variable pokemonList
     function getAll() {
-        return repository;
+        return pokemonList;
     }
 
+	// take a pokomon item and creates a html list out of them while also binding a click function to each one that will fetch more details via details api
     function addListItem(pokemon) {
         //target the class pokemon-list in the HTML file
         let pokemonList=document.querySelector(".pokemon-list");
@@ -51,46 +105,41 @@ let pokemonRepository=(function () {
         pokemonList.appendChild(listpokemon);
     }
 
-   // Event Listner functoin ot be sued later on.  
+    // in addListItem function above, we are taking a button and adding a click listener to it. 
+	// i.e, we are essentially saying, when the user will click on the button run the function loadDetail which will get the height and imgUrl
     function addPokemonListener (button, pokemon) {
         button.addEventListener('click', function() {
-                showDetails(pokemon);
-        }
-
-        )
+            loadDetail(pokemon);
+        });
     }
 
-    //Add show details function
+    //Add show details function.
+	// all this does is log the details to the console.
     function showDetails (pokemon) {
-        console.log(pokemon.name);
+        console.log(pokemon);
     }
-
+	
+	// at this point we are done. but all our functions only live inside our function. outsider will have no access.
+	// to solve this problem we are "export" all our functions so they can be called by others. 
+	// i.e, pokemonRepository.loadList().then... below lives outside this function. so if we calls pokemonRepository he will now have access to the the loadList function to do what he needs.
     return {
         //Return added items
         add: add,
         //get all items
         getAll: getAll,
         //Return the new list items
-        addListItem: addListItem
+        addListItem: addListItem,
+        loadList: loadList,
+        loadDetail:loadDetail
     }
 
     ;
 }
 
 )();
-
-//Adding new pokemon to my array
-pokemonRepository.add( {
-    name: "Kane", height: 0.5, types: ["nudnick"]
-}
-
-);
-console.log(pokemonRepository.getAll()); //log array with new item
-
-//function refrence
-pokemonRepository.getAll().forEach(function (pokemon) {
-    //Making the call to get the list items
-    pokemonRepository.addListItem(pokemon);
-}
-
-);
+// this is the first thing in the code to run, and it will trigger the list to be loaded and once loaded it will trigger the html list items to be rendered in the page
+pokemonRepository.loadList().then(function(){
+    pokemonRepository.getAll().forEach(function(item){
+        pokemonRepository.addListItem(item);
+    });
+})
